0a6f2ebba12c1cfe2de5340a02530ba3a940e7f6
erin <erin@gmail.com>
2024-01-31 22:39:40 +0000

    fix error in allowed hosts for deploy

--- codestar/codestar/settings.py
+++ codestar/codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oxnx%9a1xzgk*xnepnp$n^@+e*3&*ol+mza20%l)b!ww1gznv2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://codestar-django-blog-1-55fdb9ef7734.herokuapp.com/']
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
